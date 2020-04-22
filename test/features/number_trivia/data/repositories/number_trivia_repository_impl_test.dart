47059c9f839d795e592e28eaf50861bcbe4cc660
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-04-22 18:44:34 +0200

    📦 NEW: set up repository implementation & test

---
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -0,0 +1,55 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_tdd/core/error/failures.dart';
+import 'package:flutter_tdd/core/platform/network_info.dart';
+import 'package:flutter_tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:flutter_tdd/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:flutter_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:flutter_tdd/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:flutter_tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockRemoteDataSource extends Mock
+    implements NumberTriviaRemoteDataSource {}
+
+class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+
+void main() {
+  NumberTriviaRepositoryImpl repository;
+  MockRemoteDataSource mockRemoteDataSource;
+  MockLocalDataSource mockLocalDataSource;
+  MockNetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    mockRemoteDataSource = MockRemoteDataSource();
+    mockLocalDataSource = MockLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+
+    repository = NumberTriviaRepositoryImpl(
+        remoteDataSource: mockRemoteDataSource,
+        localDataSource: mockLocalDataSource,
+        networkInfo: mockNetworkInfo,
+        );
+  });
+
+  group('getConcreteNumberTrivia', () {
+    final tNumber = 1;
+    final tNumberTriviaModel = NumberTriviaModel(number: tNumber, text: 'test trivia');
+    final NumberTrivia tNumberTrivia = tNumberTriviaModel;
+
+    test('should check if the device is online',
+         () async {
+           when(mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+
+           repository.getConcreteNumberTrivia(tNumber);
+           
+           verify(mockNetworkInfo.isConnected);
+         }
+    );
+  });
+}
