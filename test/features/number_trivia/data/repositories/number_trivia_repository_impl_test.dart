7195fbf04738292a89bc863e8313beb579ba1f80
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-04-23 13:47:15 +0200

    👌 IMPROVE: rename platform to network, fix imports

--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_tdd/core/error/exceptions.dart';
 import 'package:flutter_tdd/core/error/failures.dart';
-import 'package:flutter_tdd/core/platform/network_info.dart';
+import 'package:flutter_tdd/core/network/network_info.dart';
 import 'package:flutter_tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_tdd/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_tdd/features/number_trivia/data/models/number_trivia_model.dart';
