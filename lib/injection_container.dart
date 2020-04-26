abbb426137145c6fa00c8ecf04a8c69632efe1e1
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-04-26 12:03:24 +0200

    📦 NEW: add dependency injection with get_it

---
+++ lib/injection_container.dart
@@ -0,0 +1,42 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_tdd/core/network/network_info.dart';
+import 'package:flutter_tdd/core/util/input_converter.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // features
+  sl.registerFactory(() =>
+      NumberTriviaBloc(concrete: sl(), random: sl(), inputConverter: sl()));
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+
+  sl.registerLazySingleton<NumberTriviaRepository>(() =>
+      NumberTriviaRepositoryImpl(
+          localDataSource: sl(), networkInfo: sl(), remoteDataSource: sl()));
+
+  sl.registerLazySingleton<NumberTriviaLocalDataSource>(
+      () => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()));
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(
+      () => NumberTriviaRemoteDataSourceImpl(client: sl()));
+  // core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  // external
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
